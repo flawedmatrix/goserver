328adf93d8f34927ccc3e8dff2eaf111cfd1c452
Edwin Xie <flawedmatrix@gmail.com>
2016-09-26 08:49:39 -0700

    Simple golang server

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/valyala/fasthttp"
+)
+
+var data string
+
+func index(ctx *fasthttp.RequestCtx) {
+	fmt.Fprintf(ctx, data)
+}
+
+func main() {
+	s := os.Getenv("RESPONSE_SIZE")
+	if s == "" {
+		s = "1"
+	}
+	responseSize, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatalf("Error parsing response size: %s", err)
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatalf("The PORT environment variable is empty")
+	}
+	data = strings.Repeat("Z", responseSize*1024)
+
+	log.Fatal(fasthttp.ListenAndServe(":"+os.Getenv("PORT"), index))
+}
